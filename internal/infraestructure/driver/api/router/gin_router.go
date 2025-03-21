71330adda67eead98c55e1653f85fac67e7a21d0
caiojorge <caio.jorge@gmail.com>
2025-03-21 16:26:26 -0300

    tentando resolver o problema de acesso ao service

--- internal/infraestructure/driver/api/router/gin_router.go
+++ internal/infraestructure/driver/api/router/gin_router.go
@@ -67,7 +67,7 @@ func corsMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-		c.Writer.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type, Accept, Authorization")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type, Accept, Authorization, Referer, User-Agent")
 		c.Writer.Header().Set("Access-Control-Expose-Headers", "Content-Length")
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 
